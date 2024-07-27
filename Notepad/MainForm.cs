2d355c17d4843d7607b129a69ec61faa65fe577f
Verbreuk Jordan <129075458+JordanKlashi@users.noreply.github.com>
2024-07-27 19:28:52 +0200

    Initial commit

---
+++ Notepad/MainForm.cs
@@ -0,0 +1,31 @@
+﻿using Notepad.Controls;
+using Notepad.Objects;
+using System;
+using System.Windows.Forms;
+
+namespace Notepad
+{
+    public partial class MainForm : Form
+    {
+        public RichTextBox CurrentRtb { get; private set; }
+        public TextFile CurrentFile { get; private set; }
+        public TabControl MainTabControl { get; private set; }
+        public Session Session { get; private set; }
+        public MainForm()
+        {
+            InitializeComponent();
+
+            var menuStrip = new MainMenuStrip();
+
+            MainTabControl = new MainTabControl();
+            Session = new Session();
+
+            Controls.AddRange(new Control[] { MainTabControl, menuStrip });
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Notepad/Objects/TextFile.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notepad.Objects
+{
+    internal class TextFile
+    {
+        /// <summary>
+        /// Chemin d'accès et nom du fichier 
+        /// </summary>
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// Chemin d'accès et nom du fichier Backup.
+        /// </summary>
+        public string BackupFileName { get; set; }
+
+        /// <summary>
+        /// Nom et exension du fichier. Le nom du fichier n'inclut pas le chemin d'accès
+        /// </summary>
+        public string SafeFileName { get; set; }
+
+        /// <summary>
+        /// Nom et extension du fichier backup. Le nom du fichier n'inclut pas le chemin d'accès
+        /// </summary>
+        public string SafeBackupFileName { get; set; }
+
+        /// <summary>
+        /// Contenu du fichier.
+        /// </summary>
+        public string Content { get; set; }
+
+        public TextFile() { }
+
+        public TextFile(string fileName)
+        {
+            FileName = fileName;
+            SafeFileName = Path.GetFileName(fileName);
+        }
+
+
+    }
+}
